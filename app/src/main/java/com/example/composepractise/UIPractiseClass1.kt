5763fc846d29e3e268d79fd705d5c25565d4431f
Sajan Tamang <sajantmg2@gmail.com>
2023-11-08 23:29:44 -0500

    first UI tutorial completed

---
+++ app/src/main/java/com/example/composepractise/UIPractiseClass1.kt
@@ -0,0 +1,91 @@
+package com.example.composepractise
+
+import android.os.Bundle
+import android.os.ParcelFileDescriptor
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composepractise.ui.theme.ComposePractiseTheme
+
+class UIPractiseClass1 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val painter = painterResource(id = R.drawable.luffy_wanted)
+            val description = "Luffy becomes a Yonkou and have awakened the devil fruit power"
+            val title = "New Wanted poster for luffy with 3000k Bounty"
+            Box(modifier = Modifier.fillMaxWidth(0.5f).padding(16.dp)) {
+                ImageCard(painter = painter, contentDescription = description, title = title)
+
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter:Painter,
+    contentDescription:String,
+    title:String,
+    modifier: Modifier=Modifier
+){
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 5.dp
+        )
+    )  {
+
+        Box(modifier = Modifier.height(200.dp)){
+            Image(painter = painter, contentDescription = contentDescription, contentScale = ContentScale.Crop)
+
+            //for the gradient
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 300f
+                    )
+                )) {
+
+            }
+
+            Box(modifier = Modifier.fillMaxSize().padding(12.dp), contentAlignment = Alignment.BottomStart) {
+                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+
+            }
+
+
+        }
+    }
+}

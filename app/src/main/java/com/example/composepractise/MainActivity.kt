3c6a2a7b1f212e6f53f1333d0ad8d20b4101f9ac
Sajan Tamang <sajantmg2@gmail.com>
2023-11-08 19:38:17 -0500

    initial commit

---
+++ app/src/main/java/com/example/composepractise/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.composepractise
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composepractise.ui.theme.ComposePractiseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Yellow),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween
+
+            ) {
+                Text(text = "Hello")
+                Text(text = "World")
+            }
+        }
+    }
+}
+

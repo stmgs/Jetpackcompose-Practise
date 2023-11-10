762c60fe82f7e7a306a4c2e0471dc137922b4c95
Sajan Tamang <sajantmg2@gmail.com>
2023-11-10 00:55:13 -0500

    state exercise finished

---
+++ app/src/main/java/com/example/composepractise/StateExercise.kt
@@ -0,0 +1,72 @@
+package com.example.composepractise
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composepractise.ui.theme.ComposePractiseTheme
+import kotlin.random.Random
+
+class StateExercise : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(modifier = Modifier.fillMaxSize()) {
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
+                }
+                ColorBox(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+
+                ){
+                    color.value=it
+                }
+
+                Box(modifier = Modifier
+                    .background(color.value)
+                    .weight(1f)
+                    .fillMaxSize()){
+
+                }
+
+            }
+
+        }
+    }
+}
+
+@Composable
+fun ColorBox(
+    modifier: Modifier=Modifier,
+    updateColor :(Color) ->Unit
+){
+
+    Box(modifier = modifier
+        .clickable {
+            updateColor(Color(
+                Random.nextFloat(),
+                Random.nextFloat(),
+                Random.nextFloat(),
+                1f
+            ))
+        }
+    ) {
+
+    }
+
+}

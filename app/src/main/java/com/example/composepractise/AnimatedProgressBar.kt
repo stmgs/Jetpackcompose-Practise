8a527460e4a572446ff1dda4512100b5596b1d00
Sajan Tamang <sajantmg2@gmail.com>
2023-11-13 22:44:37 -0500

    animated progressbar created

---
+++ app/src/main/java/com/example/composepractise/AnimatedProgressBar.kt
@@ -0,0 +1,100 @@
+package com.example.composepractise
+
+import android.os.Bundle
+import android.transition.CircularPropagation
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composepractise.ui.theme.ComposePractiseTheme
+import kotlinx.coroutines.delay
+
+class AnimatedProgressBar : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ){
+                CircularProgressBar(percentage = 0.8f, number = 200)
+            }
+        }
+    }
+}
+
+@Composable
+fun CircularProgressBar(
+    percentage : Float,
+    number: Int,
+    fontSize: TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Green,
+    strokeWidth:Dp = 8.dp,
+    animationDuration : Int = 1000,
+    animationDelay : Int = 0
+){
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+
+    val currentPercentage = animateFloatAsState(
+        targetValue = if (animationPlayed) percentage else 0f,
+        label = "",
+        animationSpec = tween(
+            durationMillis = animationDuration,
+             delayMillis = animationDelay
+        )
+    )
+
+    LaunchedEffect(key1 = true){
+        animationPlayed = true
+    }
+    
+    Box(modifier = Modifier.size(radius*2f) ,
+        contentAlignment = Alignment.Center)
+    {
+        Canvas(modifier = Modifier.size(radius * 2f) ){
+            drawArc(
+                color = color,
+                -90f,
+                360 * currentPercentage.value,
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
+        Text(
+            text = (currentPercentage.value * number).toInt().toString(),
+            color = Color.Black,
+            fontSize = fontSize
+        )
+
+
+    }
+    
+}

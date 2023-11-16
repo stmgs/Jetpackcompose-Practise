27e500a725871309e2c8e7a29bd3f0473633cd34
Sajan Tamang <sajantmg2@gmail.com>
2023-11-16 02:07:18 -0500

    Dashboard Ui created. Bottom Navigation bug remains

--- app/src/main/java/com/example/composepractise/ui/theme/Theme.kt
+++ app/src/main/java/com/example/composepractise/ui/theme/Theme.kt
@@ -1,16 +1,16 @@
-package com.plcoding.meditationuiyoutube.ui.theme
+package com.example.composepractise.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+
 import androidx.compose.runtime.Composable
 
 @Composable
-fun MeditationUIYouTubeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun MeditationUITheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
     MaterialTheme(
         typography = Typography,
-        shapes = Shapes,
+        shapes = Shapes(),
         content = content
     )
 }
